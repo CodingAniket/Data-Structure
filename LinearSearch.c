633835ccc1e296fb82a5d97b85a780082b1e9f7c
CodingAniket <magicgaming741126@gmail.com>
2025-08-04 09:26:28 +0530

    Linear Search

---
+++ LinearSearch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int LinearSerach(int arr[], int element, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[] = {1, 55, 48, 66, 2};
+    int element = 50;
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int res = LinearSerach(arr, element, size);
+    if (res == -1)
+    {
+        printf("%d is not found!", element);
+    }
+    else
+    {
+        printf("%d is found at index %d", element, res);
+    }
+}
