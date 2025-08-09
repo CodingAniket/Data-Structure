49e2afc54c829ee2c54875f915f3e9dc43873e63
CodingAniket <magicgaming741126@gmail.com>
2025-08-09 21:40:34 +0530

    ARSS_Sort

---
+++ ARSS_Sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void ArssSort(int *arr, int size)
+{
+    int element, index, swap;
+    for (int i = 0; i < size - 1; i++)
+    {
+        element = *(arr + i);
+        index = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (*(arr + i) > *(arr + j))
+            {
+                if (element > *(arr + j))
+                {
+                    element = *(arr + j);
+                    index = j;
+                }
+            }
+        }
+        swap = *(arr + i);
+        *(arr + i) = *(arr + index);
+        *(arr + index) = swap;
+    }
+    printf("Sorted Element\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n", *(arr + i));
+    }
+}
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    ArssSort(arr, size);
+    return 0;
+}
