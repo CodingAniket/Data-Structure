0827e96b7f56f41e2f0c36c3c59709de80d55ed7
CodingAniket <magicgaming741126@gmail.com>
2025-09-14 12:57:17 +0530

    Format is done

--- DeletionArray.c
+++ DeletionArray.c
@@ -1,19 +1,24 @@
-#include<stdio.h>
-void DeleteElement(int *arr,int index, int size){
-if(index<0 || index>=size){
-    printf("The index is not valid!");
-    return;
+#include <stdio.h>
+void DeleteElement(int *arr, int index, int size)
+{
+    if (index < 0 || index >= size)
+    {
+        printf("The index is not valid!");
+        return;
+    }
+    for (int i = index; i < size - 1; i++)
+    {
+        *(arr + i) = *(arr + i + 1);
+    }
+    size--;
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n", *(arr + i));
+    }
 }
-for(int i=index;i<size-1;i++){
-*(arr+i)=*(arr+i+1);
-}
-size--;
-for(int i=0;i<size;i++){
-    printf("%d\n",*(arr+i));
-}
-}
-int main(){
-    int arr[]={1,2,3,4,34,5};
-    DeleteElement(arr,4,6);
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 34, 5};
+    DeleteElement(arr, 4, 6);
     return 0;
 }
